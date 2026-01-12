f060d7b3ef0e3978e29effea628c2f51a6e7f08c
Shubhi Goyal <shubhigoyal0527@gmail.com>
2026-01-13 01:24:36 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = input.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " sorted integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the number to search: ");
+        int target = input.nextInt();
+
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
