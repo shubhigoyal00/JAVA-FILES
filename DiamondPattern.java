03df93a69b8bc095acaf34b05f2696cb7c3e7d3e
Shubhi Goyal <shubhigoyal0527@gmail.com>
2026-01-18 23:54:49 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,31 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the upper half of the diamond
+        
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
