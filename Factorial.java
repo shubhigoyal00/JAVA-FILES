0da5340c3ccb8fda96d157bbfda370a70e8c2b3a
Shubhi Goyal <shubhigoyal0527@gmail.com>
2026-01-20 18:48:56 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Factorial 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to find its factorial: ");
+        int number = input.nextInt(); 
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) 
+        {
+            factorial = factorial * i;
+        }
+        System.out.println("The factorial of " + number + " is: " + factorial);
+        input.close();
+    }
+}
