661c0f94650c9834aa5c71839f149911f77bad6c
Shubhi Goyal <shubhigoyal0527@gmail.com>
2026-01-24 18:01:07 +0530

    Add files via upload

---
+++ GradeSheet.java
@@ -0,0 +1,14 @@
+public class GradeSheet {
+    public static void main(String[] args) {
+        
+        System.out.println("*************************************************");
+        System.out.println("*          STUDENT GRADE SHEET                  *");
+        System.out.println("*************************************************");
+        System.out.println("* Name:          Aditya Singh                   *");
+        System.out.println("* Roll Number:   R2142231119                    *");
+        System.out.println("* SAP ID:        500120311                      *");
+        System.out.println("* Result:       PASS                            *");
+        System.out.println("*************************************************");
+        
+    }
+}
