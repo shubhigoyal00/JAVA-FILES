154088f43cdabeffd44e8bf54fccc283b8d2b9f2
Shubhi Goyal <shubhigoyal0527@gmail.com>
2026-01-30 22:52:27 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,61 @@
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original array:");
+        printArray(array);
+
+        mergeSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void mergeSort(int[] array) {
+
+        int mid = array.length / 2;
+        int[] left = new int[mid]; 
+        int[] right = new int[array.length - mid]; 
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = array[i];
+        }
+        for (int i = mid; i < array.length; i++) {
+            right[i - mid] = array[i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(array, left, right);
+    }
+
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+
+       
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+     
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
